5417bd57fe22e9bddafa776279389ac01e728b96
Lamonier Barbosa <lamoni_lr@hotmail.com>
2026-03-08 22:45:47 -0300

    Criando a base necessária para o inicio do projeto

---
+++ src/main/java/com/project/park_api/web/controller/UsuarioController.java
@@ -0,0 +1,57 @@
+package com.project.park_api.web.controller;
+
+import com.project.park_api.entity.Usuario;
+import com.project.park_api.service.UsuarioService;
+import com.project.park_api.web.records.UsuarioCreate;
+import com.project.park_api.web.records.UsuarioResponse;
+import com.project.park_api.web.records.UsuarioSenha;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponse> create(@RequestBody UsuarioCreate usuarioCreate){
+        Usuario user = usuarioCreate.toEntity();
+        Usuario userSaved = usuarioService.salvar(user);
+        UsuarioResponse userResponse = UsuarioResponse.fromEntity(userSaved);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> getById(@PathVariable Long id){
+        Usuario user = usuarioService.buscarPorId(id);
+        UsuarioResponse userResponse = UsuarioResponse.fromEntity(user);
+
+        return ResponseEntity.ok(userResponse);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> updatePassword(@PathVariable Long id, @RequestBody UsuarioSenha usuarioSenha){
+        Usuario user = usuarioSenha.toEntity();
+        Usuario senhaAtualizada = usuarioService.editarSenha(id, usuarioSenha.senha(), usuarioSenha.novaSenha(), usuarioSenha.confirmaSenha());
+        UsuarioResponse userResponse = UsuarioResponse.fromEntity(user);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UsuarioResponse>> getAll(){
+        List<Usuario> users = usuarioService.buscarTodos();
+        List<UsuarioResponse> responseList = users.stream().map(UsuarioResponse::fromEntity).toList();
+
+        return ResponseEntity.ok(responseList);
+    }
+}
